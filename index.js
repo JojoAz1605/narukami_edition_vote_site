810f48c1008688a3e47807796e06e34e443fd80b
Joris Masson <joris.masson2002@gmail.com>
2022-09-22 13:24:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { port } = require('./config.json');
+const parseUrl = require('body-parser');
+const fs = require("fs");
+
+const app = express();
+
+let encodeUrl = parseUrl.urlencoded({ extended: false });
+
+function log(thing) {
+    let data = fs.readFileSync("logs/access.log");
+    fs.writeFile("logs/access.log", data + "\n" + thing, function(err) {
+        if (err) return console.log(err);
+        console.log(thing);
+    });
+}
+
+
+app.use(express.static("public"));
+
+app.get('/', (request, response) => {
+    log("Demande d'accès au site par " + request.ip)
+    return response.sendFile('index.html', {root: '.'});
+});
+
+
+app.post('/', encodeUrl, (req, res) => {
+    let userID = req.body["userID"][0];
+    log("Vote envoyé par " + userID + " adresse IP: " + req.ip);
+    let voteData = ""
+    for (let i = 0; i < req.body["title"].length; i++) {
+        let noteTitre = parseInt(req.body["title"][i]);
+        let noteDesc = parseInt(req.body["description"][i]);
+        let notePhoto = parseInt(req.body["photo"][i]);
+        let data = noteTitre + noteDesc + notePhoto;
+        voteData += data.toString() + "\n";
+    fs.writeFile("scores/" + userID + ".txt", voteData, function(err) {
+        if (err) return console.log(err);
+        console.log('OUAAAAAAAIS');
+    });
+    }
+
+    res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+});
+
+app.listen(port, () => console.log(`Le serv écoute sur http://localhost:${port}`));
---
+++ public/js/func.js
@@ -0,0 +1,88 @@
+async function afficherParticipation(username) {
+    // ça sert à avoir les infos!
+    let filename = "/../participations/" + username + ".json"
+    let response = await fetch(filename);
+    let enJson = await response.json();
+
+    let leFormulaire = document.querySelector("form");  // prend le formulaire
+    let leBouton = document.querySelector("button");
+
+    // création des différents éléments
+    let nom = document.createElement("h2");
+    let title = document.createElement("h3");
+    let desc = document.createElement("p");
+    let photo = document.createElement("img");
+
+    let userIDInput = document.createElement("input");
+    let field = document.createElement("fieldset");
+    let legend = document.createElement("legend");
+
+    let labTitre = document.createElement("label");
+    let noteTitre = document.createElement("input");
+    let labDesc = document.createElement("label");
+    let noteDesc = document.createElement("input");
+    let labPhoto = document.createElement("label");
+    let notePhoto = document.createElement("input");
+    
+    // défini les attributs de chaque éléments
+    nom.innerText = username;
+    title.innerText = enJson.title;
+    desc.innerText = enJson.description;
+    photo.setAttribute("src", enJson.image);
+    photo.setAttribute("width", "400px");
+
+    userIDInput.setAttribute("name", "userID");
+    userIDInput.setAttribute("id", "userID");
+    userIDInput.setAttribute("type", "text");
+    userIDInput.style.display = "none";
+    field.setAttribute("name", username);
+    legend.innerText = username;
+
+    noteTitre.setAttribute("name", "title");
+    noteTitre.setAttribute("type", "number");
+    noteTitre.setAttribute("max", "1");
+    noteTitre.setAttribute("min", "0");
+    labTitre.setAttribute("for", "title");
+    labTitre.innerText = "Note pour le titre(0 ou 1)(c'est un point bonus): ";
+
+    noteDesc.setAttribute("name", "description");
+    noteDesc.setAttribute("type", "number");
+    noteDesc.setAttribute("max", "4");
+    noteDesc.setAttribute("min", "0");
+    labDesc.setAttribute("for", "title");
+    labDesc.innerText = "Note pour la description(de 0 à 4): ";
+
+    notePhoto.setAttribute("name", "photo");
+    notePhoto.setAttribute("type", "number");
+    notePhoto.setAttribute("max", "6");
+    notePhoto.setAttribute("min", "0");
+    labPhoto.setAttribute("for", "title");
+    labPhoto.innerText = "Note pour la photo(de 0 à 6): ";
+    
+    // affiche sur la page
+    leFormulaire.insertBefore(field, leBouton)
+    field.appendChild(userIDInput);
+    field.appendChild(legend);
+    field.appendChild(title);
+    field.appendChild(desc);
+    field.appendChild(photo);
+
+    field.appendChild(document.createElement("br"));
+
+    field.appendChild(labTitre);
+    field.appendChild(noteTitre);
+
+    field.appendChild(document.createElement("br"));
+
+    field.appendChild(labDesc);
+    field.appendChild(noteDesc);
+
+    field.appendChild(document.createElement("br"));
+
+    field.appendChild(labPhoto);
+    field.appendChild(notePhoto);
+
+    field.appendChild(document.createElement("br"));
+}
+
+// afficherParticipation("example").then(() => console.log("Ouais!"));  // ceci est un exemple, il faudra remplacer le example par le nom d'un participant pour l'afficher
